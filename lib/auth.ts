666a45b6c4137855a641ff4b9dfd26243afd24f6
Jun Sasaki <ikasas0077@gmail.com>
2025-10-06 20:41:24 +0900

    feat(db): dorizzle.config.ts を削除し、db/index.ts に authSchemas を追加してスキーマを統合しました。また、lib/auth.ts に baseURL とプラグインを追加しました。

--- lib/auth.ts
+++ lib/auth.ts
@@ -1,9 +1,14 @@
 import { betterAuth } from "better-auth";
 import { drizzleAdapter } from "better-auth/adapters/drizzle";
-import { db } from "@/db"; // your drizzle instance
+import { db } from "@/db/"; // your drizzle instance
 import {nanoid} from "nanoid"
+import { getBaseURL } from "./get-base-url";
+import {anonymous} from "better-auth/plugins"
+import { nextCookies } from "better-auth/next-js";
+
 
 export const auth = betterAuth({
+	baseURL: getBaseURL(),
     database: drizzleAdapter(db, {
         provider: "pg", 
 				// usersなどの複数形で制作してくれる
@@ -13,5 +18,7 @@ export const auth = betterAuth({
 			database: {
 				generateId: () => nanoid(10)
 			}
-		}
+		},
+		plugins:[anonymous(),nextCookies()]
+
 });
---
+++ middleware.ts
@@ -0,0 +1,22 @@
+import { NextRequest, NextResponse } from "next/server";
+import { getSessionCookie } from "better-auth/cookies";
+
+const publicRoutes = ["/login","/register","/"]
+
+export async function middleware(request: NextRequest) {
+	const sessionCookie = getSessionCookie(request);
+	const isPrivateRoute = !publicRoutes.includes(request.nextUrl.pathname)
+
+    // THIS IS NOT SECURE!
+    // This is the recommended approach to optimistically redirect users
+    // We recommend handling auth checks in each page/route
+	if (!sessionCookie && isPrivateRoute) {
+		return NextResponse.redirect(new URL("/login", request.url));
+	}
+
+	return NextResponse.next();
+}
+
+export const config = {
+	matcher: ["/((?!api|_next/static|_next/image|favicon.ico|llms\\.tst|.*\\.(png|jpg|jpeg|gif|svg|webp|ico)).*)"],
+};
